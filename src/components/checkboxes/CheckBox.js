6de137fc26f93d91e2fafa92e34c84cc97b98631
zacharysarette <zacharywsarette@gmail.com>
2020-08-25 13:01:43 +0900

    Fixes todo formatting on small screens

--- src/components/checkboxes/CheckBox.js
+++ src/components/checkboxes/CheckBox.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const CheckBox = ({ checked, onChange, label, labelClassName }) => {
   return (
-    <label>
+    <label className='checkbox__label'>
       <input
         className='checkBox'
         type='checkbox'
         checked={checked}
         onChange={() => onChange()}
       />
-      <span className={'label ' + labelClassName}>
+      <span className={'label' + labelClassName}>
         {label}
       </span>
     </label>
